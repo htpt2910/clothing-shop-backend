7eb99d6798ec4a2bfdb577b028fb097dbe846615
htpt2910 <hmc2910@gmail.com>
2025-07-21 13:58:37 +0900

    feat: add products/index,show API

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::ProductsController < ApplicationController
+  def index
+    products = Product.all.includes(:category)
+    render json: products.as_json(only: [:id, :name, :description, :price, :stock, :image_url])
+  end
+
+  def show
+    product = Product.find(params[:id])
+    render json: product.as_json(only: [:id, :name, :description, :price, :stock, :image_url])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Product not found" }, status: :not_found
+  end
+end
